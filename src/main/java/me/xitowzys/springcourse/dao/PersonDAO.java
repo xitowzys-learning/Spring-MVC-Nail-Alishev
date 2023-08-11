ed9e57d85a69843dce0711f53e5c0da323f3b06d
Kalinka George <xitowzys@gmail.com>
2023-08-11 23:32:03 +0800

    ✨ feat (Spring MVC #3): JDBC API. Базы данных.

--- src/main/java/me/xitowzys/springcourse/dao/PersonDAO.java
+++ src/main/java/me/xitowzys/springcourse/dao/PersonDAO.java
@@ -3,6 +3,7 @@ package me.xitowzys.springcourse.dao;
 import me.xitowzys.springcourse.models.Person;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
@@ -12,38 +13,81 @@ public class PersonDAO {
 
 
     private static int PEOPLE_COUNT;
-    private List<Person> people;
 
-    {
-        people = new ArrayList<>();
+    private static final String URL = "jdbc:mariadb://localhost:3306/spring_lesson";
+    private static final String USERNAME = "georgekalinka";
+    private static final String PASSWORD = "12345678";
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@gmail.com"));
 
-//        System.out.println(this.show(3).getName());
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Person> index() {
-        return people;
+        List<Person> personList = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                Person person = new Person();
+
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+
+                personList.add(person);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return personList;
     }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person(name, age, email) VALUES ('" +
+                    person.getName() + "', " + person.getAge() + ", '" + person.getEmail() + "')";
+
+            statement.executeUpdate(SQL);
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+//        person.setId(++PEOPLE_COUNT);
+//        people.add(person);
     }
 
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
     }
 
     public void update(int id, Person updatedPerson) {
-        Person personToBeUpdated = show(id);
-
-        personToBeUpdated.setName(updatedPerson.getName());
-        personToBeUpdated.setAge(updatedPerson.getAge());
-        personToBeUpdated.setEmail(updatedPerson.getEmail());
+//        Person personToBeUpdated = show(id);
+//
+//        personToBeUpdated.setName(updatedPerson.getName());
+//        personToBeUpdated.setAge(updatedPerson.getAge());
+//        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
 
@@ -55,6 +99,6 @@ public class PersonDAO {
 //            }
 //        };
 
-        people.removeIf(p -> p.getId() == id);
+//        people.removeIf(p -> p.getId() == id);
     }
 }
