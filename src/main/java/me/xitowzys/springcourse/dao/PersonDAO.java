b6e4761dffa737fdad0ec2a543ab48a6af7618b8
Kalinka George <xitowzys@gmail.com>
2023-08-12 17:59:14 +0800

    ✨ feat (Spring MVC #3): SQL инъекции. PreparedStatement. JDBC API

--- src/main/java/me/xitowzys/springcourse/dao/PersonDAO.java
+++ src/main/java/me/xitowzys/springcourse/dao/PersonDAO.java
@@ -63,11 +63,21 @@ public class PersonDAO {
 
     public void save(Person person) {
         try {
-            Statement statement = connection.createStatement();
-            String SQL = "INSERT INTO Person(name, age, email) VALUES ('" +
-                    person.getName() + "', " + person.getAge() + ", '" + person.getEmail() + "')";
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO Person(name, age, email) VALUES(?, ?, ?)"
+            );
 
-            statement.executeUpdate(SQL);
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setInt(2, person.getAge());
+            preparedStatement.setString(3, person.getEmail());
+
+            preparedStatement.executeUpdate();
+
+//            Statement statement = connection.createStatement();
+//            String SQL = "INSERT INTO Person(name, age, email) VALUES ('" +
+//                    person.getName() + "', " + person.getAge() + ", '" + person.getEmail() + "')";
+//
+//            statement.executeUpdate(SQL);
 
 
         } catch (SQLException e) {
@@ -78,11 +88,47 @@ public class PersonDAO {
     }
 
     public Person show(int id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM Person WHERE id=?"
+            );
+
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            Person person = new Person();
+
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setEmail(resultSet.getString("email"));
+            person.setAge(resultSet.getInt("age"));
+
+            return person;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 //        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-        return null;
     }
 
     public void update(int id, Person updatedPerson) {
+
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("UPDATE Person SET name=?, age=?, email=? WHERE id=?");
+
+            preparedStatement.setString(1, updatedPerson.getName());
+            preparedStatement.setInt(2, updatedPerson.getAge());
+            preparedStatement.setString(3, updatedPerson.getEmail());
+            preparedStatement.setInt(4, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
 //        Person personToBeUpdated = show(id);
 //
 //        personToBeUpdated.setName(updatedPerson.getName());
@@ -92,6 +138,15 @@ public class PersonDAO {
 
 
     public void delete(int id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Person WHERE id=?");
+
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 //        Predicate<Person> idPredicate = new Predicate<Person>() {
 //            @Override
 //            public boolean test(Person person) {
